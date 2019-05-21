5ddecb27ce8f989448a8dde786f6004c2efb2d4c
Der-Zauberer <and.som282@gmail.com>
2019-05-21 21:10:23 +0200

    Add navigation features

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function toggleMenu() {
         menu.classList.remove("responsive");
     }
 }
+
+function toggleNavigation() {
+    var navigation = document.getElementById("smart-navigation");
+    if (!navigation.classList.contains("responsive")) {
+        navigation.className += " responsive";
+        document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    } else {
+        navigation.classList.remove("responsive");
+        document.body.style.backgroundColor = "white";
+    }
+    
+}
