8fe1a79b5fe6c0312d41648559f43580b616cd9b
Der-Zauberer <and.som282@gmail.com>
2022-03-16 23:41:29 +0100

    Made menu mobile-friendly

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,6 @@
 function generateNavigation() {
-    if (document.getElementsByClassName("navigation").length) {
-        var navigation = document.getElementsByClassName("navigation")[0];
+    const navigation = document.getElementById("navigation");
+    if (navigation) {
         Array.from(document.getElementsByTagName("h2")).forEach(element => {
             if (typeof element.id != 'undefined' && !(element.id === '')) {
                 navigation.innerHTML += "<a href=\"#" + element.id + "\">" + element.innerHTML + "</a>";
@@ -9,21 +9,75 @@ function generateNavigation() {
     }
 }
 
-function toggleMenu() {
-    var menu = document.getElementById("smart-menu");
-    if (!menu.classList.contains("responsive")) {
-        menu.className += " responsive";
-    } else {
-        menu.classList.remove("responsive");
+function initializeSmartMenu() {
+    const menu = document.getElementById("smart-menu");
+    const navigation = document.getElementById("navigation");
+    if (menu) {
+        document.addEventListener("click", onMouseClick);
+        window.addEventListener('resize', onWindowResize);
+        Array.from(menu.children).forEach(element => {
+            if (!element.classList.contains("menu-title") && !(element.tagName === "IMG")) {
+                element.classList.add("not-mobile");
+            }
+        });
+    }
+    if (!navigation) {
+        menu.outerHTML += "<div id=\"navigation\" class=\"navigation only-mobile\"></div>";
     }
 }
 
-function toggleNavigation() {
-    var navigation = document.getElementById("smart-navigation");
-    if (!navigation.classList.contains("responsive")) {
-        navigation.className += " responsive";
-    } else {
-        navigation.classList.remove("responsive");
+function toggleSmartMenu() {
+    const navigation = document.getElementById("navigation");
+    let navigationContent = document.getElementsByClassName("navigation-content").item(0);
+    if (!navigationContent && document.getElementsByClassName("container").item(0)) navigationContent=document.getElementsByClassName("container").item(0);
+    else if (document.getElementsByClassName("container-fluid").item(0)) navigationContent=document.getElementsByClassName("container-fluid").item(0);
+    if (navigation) {
+        if (navigation.classList.contains("navigation-display")) {
+            navigation.classList.remove("navigation-display");
+            if (navigationContent) navigationContent.classList.remove("navigation-content-hide");
+            Array.from(navigation.children).forEach(element => {
+                if (element.classList.contains("menu-item")) navigation.removeChild(element); 
+            });
+        } else {
+            const menu = document.getElementById("smart-menu");
+            generateNavigationMenu(menu, navigation);
+            navigation.classList.add("navigation-display");
+            if (navigationContent) navigationContent.classList.add("navigation-content-hide");
+        }
+        
     }
-    
+}
+
+function onMouseClick(event) {
+    const navigation = document.getElementById("navigation");
+    if (navigation) {
+        if (navigation.classList.contains("navigation-display") && event.clientX > 250) {
+            toggleSmartMenu();
+        }
+    }
+}
+
+function onWindowResize() {
+    const navigation = document.getElementById("navigation");
+    if (navigation) {
+        if (navigation.classList.contains("navigation-display")) {
+            toggleSmartMenu();
+        }
+    }
+}
+
+function generateNavigationMenu(menu, navigation) {
+    if (menu && navigation) {
+        let menuString = "";
+        Array.from(menu.children).forEach(element => {
+            if (navigation && element.tagName === "A") {
+                let innerHtml = element.innerHTML;
+                if (element.classList.contains("menu-title")) innerHtml = "Home";
+                let classString = "only-mobile menu-item ";
+                if (element.classList.contains("menu-active")) classString += "navigation-active";
+                menuString += "<a href=\"" + element.href + "\" class=\"" + classString + "\">" + innerHtml + "</a>";
+            }
+        });
+        navigation.innerHTML = menuString + navigation.innerHTML;
+    }
 }
