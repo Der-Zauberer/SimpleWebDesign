10f29f17452af4b53c308d197e083b94e2569402
Der-Zauberer <and.som282@gmail.com>
2022-03-16 17:58:40 +0100

    Added automatic navigation

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,14 @@
+function generateNavigation() {
+    if (document.getElementsByClassName("navigation").length) {
+        var navigation = document.getElementsByClassName("navigation")[0];
+        Array.from(document.getElementsByTagName("h2")).forEach(element => {
+            if (typeof element.id != 'undefined' && !(element.id === '')) {
+                navigation.innerHTML += "<a href=\"#" + element.id + "\">" + element.innerHTML + "</a>";
+            }
+        });
+    }
+}
+
 function toggleMenu() {
     var menu = document.getElementById("smart-menu");
     if (!menu.classList.contains("responsive")) {
